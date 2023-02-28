2ac2ea3cc25548604db062822e1756f55d0b590c
Deepak Mane <thedpmane@gmail.com>
2023-02-28 21:41:51 +0530

    jwt

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -11,11 +11,11 @@ const sendToken = (user, statusCode, res) => {
     httpOnly: true,
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode).json({
     success: true,
     user,
     token,
-  });
+  }).cookie("token", token, options);
 };
 
 module.exports = sendToken;
